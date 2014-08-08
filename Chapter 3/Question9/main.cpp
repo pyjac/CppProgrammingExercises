bd8e1561b83c359e296a83ac4c4b6f5e1b7c7ac2
pyjac <oyebanji05@gmail.com>
2014-08-08 11:26:16 +0000

    ..

--- Chapter 3/Question9/main.cpp
+++ Chapter 3/Question9/main.cpp
@@ -17,13 +17,31 @@ rate per month. The program outputs the interest. Format your output to two
 decimal places.
  *
  */
+#include <iomanip>
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    double netBalance,d1,payment,interest,d2,monthlyInterestRate,averageDailyBalance;
+
+    cout << setprecision(2) << fixed << showpoint ;
+    cout << "Enter net Balance: ";
+    cin >> netBalance;
+    cout << "Enter payment made: ";
+    cin >> payment;
+    cout << "Enter number of days in the billing cycle: ";
+    cin >> d1;
+    cout << "Enter number of days payment is made before billing cycle: ";
+    cin >> d2;
+    cout  << "Enter interest per month: ";
+    cin >> monthlyInterestRate;
+
+    averageDailyBalance = (netBalance * d1 - payment * d2)/d1;
+    interest = averageDailyBalance * monthlyInterestRate;
+
+    cout <<endl<< "Interest is " << interest << endl;
     return 0;
 }
 
